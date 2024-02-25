3f7f241e1d3fb4e8632017c51b1e9e63692655de
Ilya <zaxsad109@gmail.com>
2024-02-25 10:13:45 +0300

    done

---
+++ sql part/task.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.ProductName,
+    ISNULL(c.CategoryName, 'No Category') AS CategoryName
+FROM 
+    Products p
+LEFT JOIN 
+    ProductCategories pc ON p.ProductID = pc.ProductID
+LEFT JOIN 
+    Categories c ON pc.CategoryID = c.CategoryID;
